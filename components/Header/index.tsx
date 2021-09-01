6f845bc9ca89efa87355d2674e161900ecbc2562
aridarsan <aridarsan09@gmail.com>
2021-09-01 17:03:44 +0700

    first add

---
+++ components/Header/index.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function Header(){
+	return(
+		<>
+			<header>
+				<ul>
+					<li><Link href="/"><a>Home</a></Link></li>
+					<li><Link href="/about"><a>About</a></Link></li>
+					<li><Link href="/"><a>Home</a></Link></li>
+				</ul>
+			</header>
+		</>
+	)
+}

6578ec873ed7f8fea96e4d22cf293f1bf4feb319
aridarsan <aridarsan09@gmail.com>
2021-09-03 18:24:51 +0700

    add litle

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header/index"
+import styles from "../styles/Home.module.css"
 
 export default function About(){
 	return(
 		<div>
 			<Header/>
-			<h1>About Page</h1>
+			<div className={styles.main}>
+				<h1 className={styles.title}>About Page</h1>
+			</div>
 		</div>
 	)
 }
---
+++ pages/user.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/index";
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.main}>
+        <h1 className={styles.title}>User Page</h1>
+      </div>
+    </div>
+  )
+}

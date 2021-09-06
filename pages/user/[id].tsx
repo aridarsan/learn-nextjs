963bb6fe86b37bb596e3bd3e56a2db501629762d
aridarsan <aridarsan09@gmail.com>
2021-09-06 16:36:42 +0700

    user detail add

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -2,28 +2,65 @@ import Layout from '../../components/Layout';
 import styles from '../../styles/Home.module.css';
 import { useRouter } from 'next/router';
 
-export default function User() {
-  const router = useRouter();
-  const { id } = router.query;
+
+interface User{
+  id: number,
+  name: string,
+  email: string,
+  company: string,
+  phone: string
+}
+
+interface userDetailProps{
+  user : User
+}
+
+export default function User(props: userDetailProps) {
+  const router = useRouter()
+  const {user} = props
   return (
     <Layout pageTitle='User Detail'>
-      <h3>User Page{id}</h3>
+      <h1>User Detail Page</h1>
+      <h3>Name : {user.name}</h3>
+      <p>Email : {user.email}</p>
+      <p>Company : {user.company.name}</p>
+      <p>Phone : {user.phone}</p>
+      <br/>
+      <button className={styles.btn} onClick={()=> router.push('/user')}>
+        Kembali
+      </button>
     </Layout>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const router = useRouter();
-//   const { id } = router.query;
-//   console.log(context);
-//   const resUser = await fetch(
-//     `https://jsonplaceholder.typicode.com/users/${id}`
-//   );
-//   const userDetail = await resUser.json();
+export async function getStaticPaths(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUser = await res.json();
+  const paths = dataUser.map((user: User) => ({
+    params: {
+      id: `${user.id}`
+    }
+  }));
 
-//   return {
-//     props: {
-//       userDetail,
-//     },
-//   };
-// }
+  return{
+    paths,
+    fallback: true,
+  };
+}
+
+interface GetStaticProps{
+  params: {
+    id: string
+  }
+}
+
+export async function getStaticProps(context: GetStaticProps){
+  const {id} = context.params
+      const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+  return {
+    props : {
+      user,
+    }
+  }
+}
